566b3c8d3b0fb8acfb4af66c5e753be85ae7e7fe
Francis <11583013+francisschmaltz@users.noreply.github.com>
2020-12-02 23:53:40 -0600

    Day/3 (#3)
    Solves Day 3

---
+++ day3/README.md
@@ -0,0 +1,7 @@
+# Day 3
+
+Step 1: Find the number of "trees". Basically: find the value of point on a repeating (horizontal) grid
+Step 2: Do this again but with variables
+
+1. Install nodejs
+2. Run `node index` (you'll have to change your values to use the ones for your account)
---
+++ day3/index.js
@@ -0,0 +1,60 @@
+// Load in the Data
+const treeDataRaw = require("./data");
+
+// Convert string into array where each array of
+// trees is a row (so we can repeat)
+const treeData = treeDataRaw.split(",");
+
+// Data is stored as an array of strings
+// ["...#...###......##.#..#.....##.", ...]
+// "." are free areas "#" are trees
+// strings "loop" forever
+
+/**
+ * Takes array of data and array for move
+ * and returns INT of number of
+ * trees encountered
+ * @param {Array} some array of strings
+ * @param {Number} horizontal movement
+ * @param {Number} vertical movement
+ * @return {Number} trees encountered
+ */
+const findTrees = (arr, hMove, vMove) => {
+  // Count valid passwords
+  var treeHits = 0;
+
+  // Loop through array and check each password
+  // increment int by vertical movement variable
+  for (var i = 0; i < arr.length; i += vMove) {
+    // split row string into array so we can check
+    let row = arr[i].split("");
+
+    // calculate position and make sure that
+    // non-single vertical movement is
+    // taken into account
+    let pos = (i / vMove) * hMove;
+
+    // Use remainder to alter horizontal position
+    if (pos >= row.length) {
+      pos = ((i / vMove) * hMove) % row.length;
+    }
+
+    if (row[pos] === "#") {
+      treeHits += 1;
+    }
+  }
+
+  return treeHits;
+};
+
+console.log(`Number of Tree Hits: ${findTrees(treeData, 3, 1)}`);
+
+//// Part two
+// Find trees in multiple places and multiple
+console.log(`\n\n Part Two! You need to multiply the numbers below \n\n`);
+const a = findTrees(treeData, 1, 1);
+const b = findTrees(treeData, 3, 1);
+const c = findTrees(treeData, 5, 1);
+const d = findTrees(treeData, 7, 1);
+const e = findTrees(treeData, 1, 2);
+console.log(`Number of Tree Hits: ${a * b * c * d * e}`);
