436e54b09b0e80f6efe36c96961116e45c2d2f5a
Francis Schmaltz <francis@healthjoy.com>
2020-12-06 09:42:05 -0600

    Use forEach rather than for loop as we do not need index

--- day6/index.js
+++ day6/index.js
@@ -13,14 +13,10 @@ const customsData = require("./data");
 const checkCustomsData = (arr, affirmReq = 1) => {
   var uniqueObj = {};
 
-  for (var i = 0; i < arr.length; i++) {
-    // convert string in array to array
-    let stringArr = arr[i].split("");
-
-    // loop through letters
-    for (var c = 0; c < stringArr.length; c++) {
-      let char = stringArr[c];
-
+  // Loop through strings in array
+  arr.forEach((stringArr) => {
+    // Loop through characters in string
+    stringArr.split("").forEach((char) => {
       // for every letter check if obj.letter
       // is added – if so add 1, if not set as 1
       if (typeof uniqueObj[`${char}`] === "undefined") {
@@ -28,8 +24,8 @@ const checkCustomsData = (arr, affirmReq = 1) => {
       } else {
         uniqueObj[char] += 1;
       }
-    }
-  }
+    });
+  });
 
   let affirms = 0;
 
@@ -46,15 +42,18 @@ const checkCustomsData = (arr, affirmReq = 1) => {
 };
 
 let totalAffirms = 0;
-for (var i = 0; i < customsData.length; i++) {
-  let affirms = checkCustomsData(customsData[i]);
-  totalAffirms += affirms;
-}
-console.log(`Number of affirms: ${totalAffirms}`);
-
 let totalGroupAffirms = 0;
-for (var i = 0; i < customsData.length; i++) {
-  let affirms = checkCustomsData(customsData[i], customsData[i].length);
-  totalGroupAffirms += affirms;
-}
+
+customsData.forEach((groupCustomsData) => {
+  // Part 1
+  totalAffirms += checkCustomsData(groupCustomsData);
+
+  // Part 2
+  totalGroupAffirms += checkCustomsData(
+    groupCustomsData,
+    groupCustomsData.length
+  );
+});
+
+console.log(`Number of affirms: ${totalAffirms}`);
 console.log(`Number of group affirms: ${totalGroupAffirms}`);
