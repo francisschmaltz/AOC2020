50f097b301cf59ca4f76522238a281bb4732ce60
Francis Schmaltz <francis@healthjoy.com>
2020-12-10 00:13:45 -0600

    Part 2

--- day10/index.js
+++ day10/index.js
@@ -20,8 +20,8 @@ dataSample2.forEach((int, i) => {
 });
 
 /**
- * Check if the sum of two numbers
- * in array are possible
+ * Get distance between every preceding item
+ * in array
  * @param {Array} Array of integers
  * @return {Object} Object of distances
  */
@@ -54,3 +54,47 @@ let distCount = getDistance(data);
 console.log(
   `1 differences multiplied by 3 differences: ${distCount[1] * distCount[3]}`
 );
+
+/**
+ * Get number of possible combinations
+ * of adapters
+ * @param {Array} Array of integers
+ * @return {Number} Int of combos
+ */
+const getArrangementCount = (arr) => {};
+
+/**
+ * Create every permutation of arr
+ * @param {Array} Array of integers
+ * @return {Number} Int of valid permutations
+ */
+const getValidPermutations = (arr) => {
+  // add 0 and device (highest+3) value
+  // to array
+  arr.unshift(0);
+  arr.push(arr[arr.length - 1] + 3);
+
+  console.log(arr);
+
+  // create new array that's empty until we add
+  // values to it
+  let validPermutations = new Array(arr.length);
+  validPermutations.fill(0, 0, arr.length);
+  validPermutations[0] = 1;
+
+  // For each value in array check what possible
+  // outcomes we have
+  arr.forEach((value, i) => {
+    for (let j = i + 1; j < arr.length; j++) {
+      // If next value is less than three, add it to valid perm
+      if (arr[j] - value <= 3) {
+        validPermutations[j] += validPermutations[i];
+      }
+    }
+  });
+
+  console.log(validPermutations);
+  return validPermutations[validPermutations.length - 1];
+};
+
+console.log(`Valid Permutations of Array: ${getValidPermutations(data)}`);
