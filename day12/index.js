22d56dc9239ffad5844a387b330206ea3a201904
Francis Schmaltz <francis@healthjoy.com>
2020-12-12 00:11:39 -0600

    part two

--- day12/index.js
+++ day12/index.js
@@ -3,6 +3,22 @@ const data = require("./data").split("\n");
 const dataSample1 = require("./dataSample1").split("\n");
 
 /**
+ * internal func to parse instruction and return parsed input
+ * @param {String} String for insctruction
+ * @return {Object} Object of action and value for move
+ */
+const _parseInst = (inst) => {
+  // Get action - first letter (always 1 char)
+  let action = inst[0];
+
+  // Create INT for move – all other chars
+  let value = parseInt(inst.substring(1));
+
+  // return as obj
+  return { action: action, value: value };
+};
+
+/**
  * Takes a list of moves and translated them to position.
  * The position is an array of figures and a direction. The
  * positon array is [Int, Int] with [0,0] being starting. The
@@ -21,23 +37,6 @@ changePos = (arr, startingPos = [0, 0], startingDir = 90) => {
   let posNS = startingPos[0];
   let posEW = startingPos[1];
   let dirDg = startingDir;
-
-  //
-  /**
-   * internal func to parse instruction and return parsed input
-   * @param {String} String for insctruction
-   * @return {Object} Object of action and value for move
-   */
-  const _parseInst = (inst) => {
-    // Get action - first letter (always 1 char)
-    let action = inst[0];
-
-    // Create INT for move – all other chars
-    let value = parseInt(inst.substring(1));
-
-    // return as obj
-    return { action: action, value: value };
-  };
 
   arr.forEach((move, i) => {
     // parse object into object with values
@@ -99,5 +98,129 @@ changePos = (arr, startingPos = [0, 0], startingDir = 90) => {
 let endPos = changePos(data);
 console.log(endPos);
 console.log(
-  `Manhattan Dist: ${Math.abs(endPos.posNS) + Math.abs(endPos.posEW)}`
+  `Part One - Manhattan Dist: ${
+    Math.abs(endPos.posNS) + Math.abs(endPos.posEW)
+  }`
+);
+
+//// Part 2
+
+/**
+ * Takes a list of moves and translated them to position.
+ * The position is an array of figures and a direction. The
+ * positon array is [Int, Int] with [0,0] being starting. The
+ * positive is north and east, respectively.
+ * @param {Array} Array for moves
+ * @param {Array} Array - Optional – waypoint pos default
+ * to [0,0]
+ * @param {Array} Array - Optional – ship pos default
+ * to [0,0]
+ * @return {Number} position from 1 to 2^(string length)
+ */
+changeWayPoint = (arr, waypointPos = [1, 10], shipPos = [0, 0]) => {
+  // Create initial direction values
+  // This also prevents stupid JS
+  // array clone issues
+  // the ship posistion is abosolute
+  let posNS = shipPos[0];
+  let posEW = shipPos[1];
+
+  // The waypoint pos is relative
+  let waypointPosNS = waypointPos[0];
+  let waypointPosEW = waypointPos[1];
+
+  arr.forEach((move, i) => {
+    // parse object into object with values
+    let instruction = _parseInst(move);
+
+    switch (instruction.action) {
+      case "N":
+        // move waypoint north
+        waypointPosNS += instruction.value;
+        break;
+      case "S":
+        // move waypoint south
+        waypointPosNS -= instruction.value;
+        break;
+      case "E":
+        // move waypoint east
+        waypointPosEW += instruction.value;
+        break;
+      case "W":
+        // move waypoint west
+        waypointPosEW -= instruction.value;
+        break;
+      case "L":
+        // change direction waypoint is facing
+        // from ship to about turning left
+        if (instruction.value === 90) {
+          // create tmp values
+          let _ns = waypointPosNS;
+          let _ew = waypointPosEW;
+
+          waypointPosNS = _ew;
+          waypointPosEW = _ns * -1;
+        } else if (instruction.value === 180) {
+          waypointPosNS = waypointPosNS * -1;
+          waypointPosEW = waypointPosEW * -1;
+        } else if (instruction.value === 270) {
+          // create tmp values
+          let _ns = waypointPosNS;
+          let _ew = waypointPosEW;
+
+          waypointPosNS = _ew * -1;
+          waypointPosEW = _ns;
+        } else {
+          throw new Error(`Unknown degree: ${instruction.value}`);
+        }
+
+        break;
+      case "R":
+        // change direction waypoint is facing
+        // from ship to about turning left
+        if (instruction.value === 90) {
+          // create tmp values
+          let _ns = waypointPosNS;
+          let _ew = waypointPosEW;
+
+          waypointPosNS = _ew * -1;
+          waypointPosEW = _ns;
+        } else if (instruction.value === 180) {
+          waypointPosNS = waypointPosNS * -1;
+          waypointPosEW = waypointPosEW * -1;
+        } else if (instruction.value === 270) {
+          // create tmp values
+          let _ns = waypointPosNS;
+          let _ew = waypointPosEW;
+
+          waypointPosNS = _ew;
+          waypointPosEW = _ns * -1;
+        } else {
+          throw new Error(`Unknown degree: ${instruction.value}`);
+        }
+        break;
+      case "F":
+        // use waypoint relative value as move
+        // instructions for each value
+        posNS += waypointPosNS * instruction.value;
+        posEW += waypointPosEW * instruction.value;
+
+        break;
+      default:
+        // We shouldn't have a default so give error
+        console.log(instruction.action);
+        throw new Error("No case met – error");
+    }
+  });
+
+  // return object of points and directions
+  return { posNS: posNS, posEW: posEW };
+};
+
+let endPosRel = changeWayPoint(data);
+console.log(endPosRel);
+console.log(
+  `Part Two - Manhattan Dist: ${
+    Math.abs(endPosRel.posNS) + Math.abs(endPosRel.posEW)
+  }`
 );
